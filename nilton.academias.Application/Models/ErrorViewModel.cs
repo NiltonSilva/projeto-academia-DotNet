a6b606cc020f9b7387b8c02bebcb6b89883a6e35
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-01-05 23:05:03 -0300

    first commit

---
+++ nilton.academias.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nilton.academias.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ nilton.academias.Infra.Data/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nilton.academias.Domain.Entities.Account;
+
+namespace nilton.academias.Infra.Data.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {  }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        { 
+            base.OnModelCreating(builder);
+        }
+    }
+}
