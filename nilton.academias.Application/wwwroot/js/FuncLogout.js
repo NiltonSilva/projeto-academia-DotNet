47f866750949cd7f75f35bb3e09a9bc68b44d2e0
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-02-06 22:15:26 -0300

    realizado correção no logout

--- nilton.academias.Application/wwwroot/js/FuncLogout.js
+++ nilton.academias.Application/wwwroot/js/FuncLogout.js
@@ -1,1 +1,17 @@
-﻿
+﻿$('.btnSair').click(function () {
+    logout();
+})
+
+function logout() {
+    if (confirm("Deseja sair realmente?")) {
+        var Url = "/Account/LogOut";
+        $.ajax({
+            typeof: "POST",
+            url: Url,
+            success: function () {
+            }
+        });
+        alert("Usuário desconectado com sucesso!");
+        window.location.href = "/Home/Index";
+    }
+}
